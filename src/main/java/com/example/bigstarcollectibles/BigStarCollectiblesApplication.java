3e5ded925f4145215e8a044628eb1379e89e063a
Walter Bernal <wltrbm@gmail.com>
2024-04-02 12:12:08 -0700

    fix: initial config

---
+++ src/main/java/com/example/bigstarcollectibles/BigStarCollectiblesApplication.java
@@ -0,0 +1,13 @@
+package com.example.bigstarcollectibles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigStarCollectiblesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigStarCollectiblesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bigstarcollectibles/BigStarCollectiblesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bigstarcollectibles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigStarCollectiblesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
