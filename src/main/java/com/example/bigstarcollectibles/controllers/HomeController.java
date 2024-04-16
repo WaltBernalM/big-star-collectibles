f6a0c7f3aadf2b710cdcd91e6acff23776fd81fa
Walter Bernal <wltrbm@gmail.com>
2024-04-16 16:04:46 -0700

    feat: implementation of fragments and home controller

---
+++ src/main/java/com/example/bigstarcollectibles/controllers/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.bigstarcollectibles.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String displayHome(Model model) {
+        return "index";
+    }
+
+    @GetMapping("/getCharacter/{charname}")
+    public String getCharacter(@PathVariable("charname") String charName) {
+        return "/characters/" + charName;
+    }
+}
