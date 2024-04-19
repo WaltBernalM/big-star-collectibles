f9dc61a1f034a620faf2ec34e9a46a658ce15d77
Walter Bernal <wltrbm@gmail.com>
2024-04-19 04:42:33 -0700

    feat: implementation of Products

--- src/main/java/com/example/bigstarcollectibles/repository/jpa/ProductRepositoryJpa.java
+++
@@ -1,9 +0,0 @@
-package com.example.bigstarcollectibles.repository.jpa;
-
-import com.example.bigstarcollectibles.dto.resource.ProductResource;
-
-import java.util.List;
-
-public interface ProductRepositoryJpa {
-    List<ProductResource> searchByName(String name);
-}
