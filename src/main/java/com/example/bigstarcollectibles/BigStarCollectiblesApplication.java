455bb9a5f61e48e73fa819f586ba45335c784bdc
Walter Bernal <wltrbm@gmail.com>
2024-04-19 05:13:32 -0700

    fix: app not starting

--- src/main/java/com/example/bigstarcollectibles/BigStarCollectiblesApplication.java
+++ src/main/java/com/example/bigstarcollectibles/BigStarCollectiblesApplication.java
@@ -2,10 +2,9 @@ package com.example.bigstarcollectibles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BigStarCollectiblesApplication extends SpringBootServletInitializer {
+public class BigStarCollectiblesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BigStarCollectiblesApplication.class, args);
